86e02f3682925ec9762f52b005cc2fd0f9b570d1
Yonatan Coti <yongc998@gmail.com>
2017-05-23 09:42:44 -0600

    leida y carga de texto

--- Proyecto1/Default.aspx
+++ Proyecto1/Default.aspx
@@ -3,14 +3,16 @@
 <asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
 
     <div class="jumbotron">
-        <h1>ASP.NET</h1>
-        <p class="lead">ASP.NET is a free web framework for building great Web sites and Web applications using HTML, CSS, and JavaScript.</p>
-        <p><a href="http://www.asp.net" class="btn btn-primary btn-lg">Learn more &raquo;</a></p>
+        <h1>Comunidad</h1>
+        <p>&nbsp;</p>
     </div>
 
     <div class="row">
         <div class="col-md-4">
-            <h2>Getting started</h2>
+            <h2>estadisticas<asp:GridView ID="GridView1" runat="server">
+                </asp:GridView>
+                <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Mostrar" />
+            </h2>
             <p>
                 ASP.NET Web Forms lets you build dynamic websites using a familiar drag-and-drop, event-driven model.
             A design surface and hundreds of controls and components let you rapidly build sophisticated, powerful UI-driven sites with data access.
--- Proyecto1/Default.aspx.cs
+++ Proyecto1/Default.aspx.cs
@@ -4,12 +4,93 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.IO;
+using System.Reflection;
 
 namespace Proyecto1
 {
     public partial class _Default : Page
     {
+        List<comunidad> listacomunidad = new List<comunidad>();
+        List<datospropietarios> listadatospropiedad = new List<datospropietarios>();
+        List<gastos> listagastos = new List<gastos>();
+        List<gastos2> listagastos2 = new List<gastos2>();
+        List<loca> listalocal = new List<loca>();
+        List<piso> listapiso = new List<piso>();
+        List<Garaje> listagaraj = new List<Garaje>();
+
+        public void  cargando (bool a, bool b, bool c) {
+
+       
+            if (a == true)
+            {
+                Assembly _assembly; Stream _imageStream;
+                StreamReader _textStreamReader;
+                _assembly = Assembly.GetExecutingAssembly();
+                _imageStream = _assembly.GetManifestResourceStream("Proyecto1.comunidad.bmp");
+                _textStreamReader = new StreamReader(_assembly.GetManifestResourceStream("examenrepaso2.comunidad.txt"));
+
+                while (_textStreamReader.Peek() > -1)
+                {
+                    comunidad comutemp = new comunidad();
+                    comutemp.Identificacion = _textStreamReader.ReadLine();
+                    comutemp.Nombre = _textStreamReader.ReadLine();
+                    comutemp.Nombre =_textStreamReader.ReadLine();
+                    listacomunidad.Add(comutemp);
+                }
+                _textStreamReader.Close();
+            }
+            if (b == true)
+            {
+                Assembly _assembly; Stream _imageStream;
+                StreamReader _textStreamReader;
+                _assembly = Assembly.GetExecutingAssembly();
+                _imageStream = _assembly.GetManifestResourceStream("Proyecto1.datospropietarios.bmp");
+                _textStreamReader = new StreamReader(_assembly.GetManifestResourceStream("examenrepaso2.datospropietarios.txt"));
+
+                while (_textStreamReader.Peek() > -1)
+                {
+                    datospropietarios datostemp = new datospropietarios();
+                    datostemp.Nombre = _textStreamReader.ReadLine();
+                    datostemp.Nit = _textStreamReader.ReadLine();
+                    datostemp.Email = _textStreamReader.ReadLine();
+
+
+                    listadatospropiedad.Add(datostemp);
+                }
+                _textStreamReader.Close();
+            }
+            if (c == true)
+            {
+                Assembly _assembly; Stream _imageStream;
+                StreamReader _textStreamReader;
+                _assembly = Assembly.GetExecutingAssembly();
+                _imageStream = _assembly.GetManifestResourceStream("Proyecto1.gastos.bmp");
+                _textStreamReader = new StreamReader(_assembly.GetManifestResourceStream("examenrepaso2.gastos.txt"));
+
+                while (_textStreamReader.Peek() > -1)
+                {
+                    gastos gastostemp = new gastos();
+                    gastostemp.Identifacion = _textStreamReader.ReadLine();
+                    gastostemp.Nombre = _textStreamReader.ReadLine();
+                    gastostemp.Tipoderepart =_textStreamReader.ReadLine();
+                    
+                    listagastos.Add(gastostemp);
+                }
+                _textStreamReader.Close();
+            }
+            //trmino 1 lista
+
+
+
+        }
+
         protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
         {
 
         }
